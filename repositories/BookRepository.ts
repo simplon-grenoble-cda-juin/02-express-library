8732dac03da499d1f45c861b75c05bb9faa4e6b1
Kew <wolff.kevin@protonmail.com>
2025-09-02 11:32:39 +0200

    Work of tuesday

--- repositories/BookRepository.ts
+++ repositories/BookRepository.ts
@@ -3,27 +3,33 @@ import { Book } from "../models/Book";
 
 export class BookRepository extends Repository {
   // Récupère tous les livres
-  async findAll() {
+  async findAll(): Promise<Book[]> {
     const query = {
       name: "fetch-all-book",
       text: `SELECT * FROM book`,
     };
 
-    // [1] Soumission de la requête à la base de données
-    const result = await this.pool.query(query);
+    try {
+      // [1] Soumission de la requête à la base de données
+      const result = await this.pool.query(query);
 
-    // [2] Transforme les données brutes en objets `Book`
-    const clearResult = await result.rows.map((row) => {
-      return new Book(
-        row.id,
-        row.title,
-        row.publisher_id,
-        row.category_id,
-        row.publication_year
-      );
-    });
+      // [2] Transforme les données brutes en objets `Book`
+      const clearResult = result.rows.map((row) => {
+        return new Book(
+          row.id,
+          row.title,
+          row.publisher_id,
+          row.category_id,
+          row.publication_year
+        );
+      });
 
-    // [3] Retourne une promesse d'un tableau de `Book`
-    return clearResult;
+      // [3] Retourne une promesse d'un tableau de `Book`
+      return clearResult;
+    } catch (error) {
+      return [];
+    }
   }
+
+  findById() {}
 }
